15da5d34fea2914ef5b3c8f518dc2d68c536b30b
sandrobispo <sbispo@otimaeg.com.br>
2023-06-28 10:26:04 -0300

    ajuste no allowed host

--- djangosige/configs/settings.py
+++ djangosige/configs/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
+ALLOWED_HOSTS = ['*']
 
 if not DEFAULT_DATABASE_URL:
     DEFAULT_DATABASE_URL = 'sqlite:///' + os.path.join(APP_ROOT, 'db.sqlite3')
