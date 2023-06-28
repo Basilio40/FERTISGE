449cd6e0559bff079d84aac636c1f0103d23adaf
sandrobispo <sbispo@otimaeg.com.br>
2023-06-28 18:20:37 -0300

    ajuste no settings

--- djangosige/configs/settings.py
+++ djangosige/configs/settings.py
@@ -154,7 +154,6 @@ MEDIA_URL = 'media/'
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 
 LOGIN_NOT_REQUIRED = (
-    r'^/login/$',
     r'/login/esqueceu/',
     r'/login/trocarsenha/',
     r'/logout/',
