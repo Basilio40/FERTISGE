85c6a00111febf66790dd58953d23e873a3289ee
vicinaldo83 <higormatosvici@gmail.com>
2023-08-31 14:33:36 -0300

    fin faturamento

--- djangosige/static/js/graphrender.js
+++ djangosige/static/js/graphrender.js
@@ -9,7 +9,7 @@ const test_data = {
         {
             data: ["2023-08-13", "2023-08-19"],
             titulo: "Entradas genérica 2",
-            valor: "2000.00",
+            valor: "200.00",
             saldo_final: "0.0",
         },
         {
@@ -35,34 +35,64 @@ const test_data = {
         {
             data: ["2023-08-06", "2023-08-12",],
             titulo: "Despesa genérica 1",
-            valor: "-1000.00",
+            valor: "1000.00",
             saldo_final: "0.0",
         },
         {
             data: ["2023-08-13", "2023-08-19"],
             titulo: "Despesa genérica 2",
-            valor: "-2000.00",
+            valor: "2000.00",
             saldo_final: "0.0",
         },
         {
             data: ["2023-08-20", "2023-08-26"],
             titulo: "Despesa genérica 3",
-            valor: "-3000.00",
+            valor: "3000.00",
             saldo_final: "0.0",
         },
         {
             data: ["2023-08-27", "2023-09-02"],
             titulo: "Despesa genérica 4",
-            valor: "-4000.00",
+            valor: "4000.00",
             saldo_final: "0.0",
         },
         {
             data: ["2023-09-03", "2023-09-10"],
             titulo: "Despesa genérica 5",
-            valor: "-5000.00",
+            valor: "500.00",
             saldo_final: "0.0",
         },
     ],
+    categorias: {
+        receitas: [
+            {
+                titulo: "BOLETO",
+                valor: "172052.00",
+            },
+            {
+                titulo: "TRANSFERENCIA",
+                valor: "159982.00",
+            },
+            {
+                titulo: "ACERTO",
+                valor: " 1000.00",
+            },
+        ],
+        despesas: [
+            {
+                titulo: "FRETE",
+                valor: "68062.00",
+            },
+            {
+                titulo: "Financiamento/Compra de Veic...",
+                valor: " 57708.75",
+            },
+            {
+                titulo: "Gastos Cartão de Crédito Empres...",
+                valor: "53641.57",
+            },
+        ],
+    }
 };
 
 // Manipulador para configurar os dados para cada tipo de gráfico
@@ -78,6 +108,12 @@ class RxD_item {
 
     set_data(data) {
         this.mov_info = data;
+        let total = (() => { let r = 0; for (let i = 0; i < data.length; i++) { r += Number(data[i].valor) } return r; })();
+
+        for (let i = 0; i < this.mov_info.length; i++) {
+            this.mov_info[i].percentual = (Number(this.mov_info[i].valor) / total)
+                .toLocaleString('pt-br', { style: 'percent', minimumFractionDigits: 2 });
+        }
         return this;
     }
 
@@ -96,7 +132,7 @@ class RxD_item {
 
     map_doughnut_xaxis() {
         let out = [];
-        
+
         for (let i = 0; i < this.mov_info.length; i++) {
             out.push(this.mov_info[i].titulo);
         }
@@ -119,7 +155,7 @@ class RxD_item {
 
     map_bar_xaxis() {
         let out = [];
-        
+
         for (let i = 0; i < this.mov_info.length; i++) {
             out.push(this.format_date(new Date(this.mov_info[i].data[0])));
         }
@@ -127,20 +163,20 @@ class RxD_item {
         return out;
     }
 
-    brighter_color(color, ratio=0.1) {
+    brighter_color(color, ratio = 0.1) {
         return color;
     }
 
     format_date(date) {
         let dia = date.toLocaleString('pt-br', { day: '2-digit' });
-        let mes = date.toLocaleString('pt-br', {month: 'short'}).split('.')[0];
+        let mes = date.toLocaleString('pt-br', { month: 'short' }).split('.')[0];
 
         return `${dia} ${mes[0].toUpperCase() + mes.substring(1)}`
     };
 }
 
 // Classe usados para configurar cada DataSet das tabelas
-class Receitas extends RxD_item{
+class Receitas extends RxD_item {
 
     constructor(data) {
         super("Receitas", "#129e3f")
@@ -149,7 +185,7 @@ class Receitas extends RxD_item{
 }
 
 // Classe usados para configurar cada DataSet das tabelas
-class Despesas extends RxD_item{
+class Despesas extends RxD_item {
 
     constructor(data) {
         super("Despesas", "#d72638")
@@ -165,7 +201,7 @@ function get_csrf() {
 
 // Coleta as informações necessário no banco e constroi os gráficos e tabelas
 function get_data(filter) {
-    url= "/financeiro/lancamentos/graphdata/"    
+    url = "/financeiro/lancamentos/graphdata/"
 
     fetchOptions = {
         method: "POST",
@@ -186,7 +222,7 @@ function get_data(filter) {
 }
 
 function response_handler(json) {
-    
+
 }
 
 // Renderiza o gráfico
@@ -196,15 +232,29 @@ function render(data) {
         graph_categorias_receitas: document.getElementById('graph-categorias-receitas').getContext('2d'),
         graph_categorias_despesas: document.getElementById('graph-categorias-despesas').getContext('2d'),
     }
-    
 
-    const receitas = new Receitas(data.entradas)
-    const despesas = new Despesas(data.saidas)
-    const default_options = (obj) => {
 
+    let receitas = new Receitas(data.entradas),
+        despesas = new Despesas(data.saidas),
+        cat_receitas = new Receitas(data.categorias.receitas),
+        cat_despesas = new Despesas(data.categorias.despesas);
+
+    const tooltip_callbacks = {
+        title: function (context_li) {
+            let context = context_li[0];
+            return context.dataset.mov_info[context.dataIndex].titulo;
+        },
+        label: function (context) {
+            let prcnt_str = "Percentual: " +
+                `${context.dataset.mov_info[context.dataIndex].percentual
+                    .toLocaleString('pt-br', { style: 'percent', minimumFractionDigits: 2 })}`;
+
+            return prcnt_str;
+        }
     }
 
-    const bar_graph_financeiro = new Chart(context.rece_desp, {
+
+    bar_graph_financeiro = new Chart(context.rece_desp, {
         type: 'bar',
         data: {
             labels: receitas.map_bar_xaxis(),
@@ -220,35 +270,38 @@ function render(data) {
         }
     });
 
-    const doughnut_graph_financeiro_r = new Chart(context.graph_categorias_receitas,  {
+    doughnut_graph_financeiro_r = new Chart(context.graph_categorias_receitas, {
         type: 'doughnut',
-        data: { datasets: [receitas.map_doughnut_yaxis()] },
+        data: { datasets: [cat_receitas.map_doughnut_yaxis()] },
         options: {
             plugins: {
                 legend: {
                     title: {
                         display: true,
-                        text: receitas.label,
+                        text: cat_receitas.label,
                         font: {
                             size: "20px",
                             weight: '500',
                             family: "Ruda, sans-serif",
                         }
-                    }
-                }
+                    },
+                },
+                tooltip: {
+                    callbacks: tooltip_callbacks
+                },
             }
         }
     })
 
-    const doughnut_graph_financeiro_d = new Chart(context.graph_categorias_despesas,  {
+    doughnut_graph_financeiro_d = new Chart(context.graph_categorias_despesas, {
         type: 'doughnut',
-        data: { datasets: [despesas.map_doughnut_yaxis()] },
+        data: { datasets: [cat_despesas.map_doughnut_yaxis()] },
         options: {
             plugins: {
                 legend: {
                     title: {
                         display: true,
-                        text: despesas.label,
+                        text: cat_despesas.label,
                         font: {
                             size: "20px",
                             weight: '500',
@@ -262,142 +315,263 @@ function render(data) {
 }
 
 
+
+// Base para os geradores de cada tabela
 class table_base {
     data = [];
+    format = {};
     n_rows = 0;
     style = {
         neg: "#d72638",
         pos: "#129e3f",
     }
 
-    constructor(n_rows, color=undefined) {
-        this.data = this.set_data(data);
+    constructor(n_rows, color = undefined) {
         this.n_rows = n_rows;
-        
+
         if (color) {
             this.style = color;
         }
     }
 
-    set_data(data) {
-        self.data = data;
-        return this;
-    }
-
     create_row(format, data) {
         let tr = document.createElement("tr");
+        tr.role = "row";
+
         for (let i = 0; i < this.n_rows; i++) {
             let td = document.createElement("td");
-            let f_data = format[i](data);
-            
+            let f_data = format[`i${i}`](data);
+
             if (f_data.style) {
                 td.style = f_data.style;
             }
-            td.textContent = f_data.out
+            td.textContent = f_data.value
+
 
             tr.appendChild(td);
         }
 
-        return tr
+        return tr;
+    }
+
+    create_table(table, dataIndex = undefined) {
+        if (dataIndex) {
+            for (let i = 0; i < this.data[dataIndex].length; i++) {
+                table.tBodies[0].appendChild(this.create_row(this.format, this.data[dataIndex][i]));
+            }
+        }
+        else {
+            for (let i = 0; i < this.data.length; i++) {
+                table.tBodies[0].appendChild(this.create_row(this.format, this.data[i]));
+            }
+        }
+    }
+
+    create_footer(table, dataIndex = undefined) {
+        table.tFoot = document.createElement("tfoot");
+        if (dataIndex) {
+            table.tFoot.appendChild(this.create_row(this.format, this.data.footer[dataIndex]));
+        }
+        else {
+            table.tFoot.appendChild(this.create_row(this.format, this.data.footer));
+        }
+        table.tFoot.firstChild.firstChild.style.textAlign = "left";
+        table.tFoot.firstChild.lastChild.style.textAlign = "right";
     }
 
     format_date(date) {
         let dia = date.toLocaleString('pt-br', { day: '2-digit' });
-        let mes = date.toLocaleString('pt-br', {month: 'short'}).split('.')[0];
+        let mes = date.toLocaleString('pt-br', { month: 'short' }).split('.')[0];
 
-        return `${dia} ${mes[0].toUpperCase() + mes.substring(1)}`
-    };
-}
-
-class table_detalhamento extends table_base{
-
-    format = {
-        0: function (data) {
-            let ini = this.format_date(new Date(data[0])),
-            fim = this.format_date(new Date(data[1]));
-
-            return {out: `${ini} à ${fim}`};
-        },
-        1: function (data) {
-            let out = {};
-            out.out = data.entrada;
-            out.style = `color:${(out.out < 0)? this.color.neg : this.color.pos}`;
-
-            return out;
-        },
-        2: function (data) {
-            let out = {};
-            out.out = data.saida;
-            out.style = `color:${(out.out < 0)? this.color.neg : this.color.pos}`;
-
-            return out;
-        },  
-        4: function (data) {
-            let out = {};
-            out.out = data.valor
-            out.style = `color:${(out.out < 0)? this.color.neg : this.color.pos}`;
-            
-            return out;
-        },
+        return `${dia} ${mes[0].toUpperCase() + mes.substring(1)}`;
     }
 
-    constructor() {
+    format_number(n) {
+        return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(n.toFixed(2));
+    }
+
+    format_number_pct(n) {
+        return (n).toLocaleString('pt-br', { style: 'percent', minimumFractionDigits: 2 });
+    }
+}
+
+// Classa usada para configurar DataSet da table "Detalhamento"
+class table_detalhamento extends table_base {
+    constructor(data) {
         super(4)
+        this.set_data(data);
+        this.set_format();
     }
 
-    create_table(table) {
-        for( let i = 0; i < this.data.length; i++) {
-            table.tBodies[0].appendChild(this.create_row(this.format, this.data[i]));
+
+    set_data(data) {
+        let receitas = data.entradas,
+            despesas = data.saidas;
+
+        let i;
+        for (i = 0; i < receitas.length; i++) {
+            let n_data = {};
+            n_data.data = receitas[i].data;
+            n_data.entrada = Number(receitas[i].valor);
+            n_data.saida = -1 * Number(despesas[i].valor);
+            n_data.valor = n_data.entrada + n_data.saida;
+
+            this.data.push(n_data);
         }
+
+        this.data.footer = [];
+        return this;
     }
 
+    set_format() {
+
+        let format = function () { };
+
+        format.i0 = function (data) {
+            let ini, fim;
+            ini = this.format_date(new Date(data.data[0]));
+            fim = this.format_date(new Date(data.data[1]));
+
+            return { value: `${ini} à ${fim}` };
+        };
+        format.i1 = function (data) {
+            let out = {};
+            out.value = this.format_number(data.entrada);
+
+            return out;
+        };
+        format.i2 = function (data) {
+            let out = {};
+            out.value = this.format_number(data.saida);
+            if (data.saida != 0) {
+                out.style = `color:${(data.saida < 0) ? this.color.neg : this.color.pos}`;
+            }
+
+            return out;
+        };
+        format.i3 = function (data) {
+            let out = {};
+            out.value = this.format_number(data.valor);
+            if (data.valor != 0) {
+                out.style = `color:${(data.valor < 0) ? this.color.neg : this.color.pos}`;
+            }
+
+            return out;
+        };
+        format.format_date = this.format_date;
+        format.format_number = this.format_number;
+        format.color = this.style;
+        this.format = format;
+
+        return this;
+    }
 }
 
-class table_categorias extends table_base{
-    format = {
-        0: function (data) {
-            return data.title
-        },
-        1: function (data) {
-            return data.entrada
-        },
-        2: function (data) {
-            return data.saida
-        },  
-        4: function (data) {
-            return data.total
-        },
-        5: function (data) {
-            return data
-        },
+// Classa usada para configurar DataSet da table "Receitas" e "Despesas" em "Categorias"
+class table_categorias extends table_base {
+    constructor(data) {
+        super(3)
+        this.set_data(data);
+        this.set_format();
     }
-    constructor(data){
-        super(4, "#d72638")
+
+    set_data(data) {
+        let receitas = data.categorias.receitas,
+            despesas = data.categorias.despesas;
+        let i;
+
+        this.data.receitas = [];
+        this.data.despesas = [];
+
+        let total_receitas = (() => { let r = 0; for (i = 0; i < receitas.length; i++) { r += Number(receitas[i].valor) } return r; })(),
+            total_despesas = (() => { let r = 0; for (i = 0; i < despesas.length; i++) { r += Number(despesas[i].valor) } return r; })();
+
+        for (i = 0; i < receitas.length; i++) {
+            let n_data = {};
+            n_data.titulo = receitas[i].titulo;
+            n_data.valor = Number(receitas[i].valor);
+            n_data.percentual = n_data.valor / total_receitas;
+
+            this.data.receitas.push(n_data);
+        }
+
+        for (i = 0; i < despesas.length; i++) {
+            let n_data = {};
+            n_data.titulo = despesas[i].titulo;
+            n_data.valor = -1 * Number(despesas[i].valor);
+            n_data.percentual = -1 * n_data.valor / total_despesas;
+
+            this.data.despesas.push(n_data);
+        }
+
+        this.data.footer = {
+            receitas: {
+                titulo: "Total",
+                valor: total_receitas,
+            },
+            despesas: {
+                titulo: "Total",
+                valor: -1 * total_despesas,
+            }
+        }
+        return this;
+    }
+
+    set_format() {
+
+        let format = function () { };
+
+        format.i0 = function (data) {
+            return { value: data.titulo };
+        };
+        format.i1 = function (data) {
+            let out = {};
+            if (data.percentual) {
+                out.value = this.format_number_pct(data.percentual);
+            }
+
+            return out;
+        };
+        format.i2 = function (data) {
+            let out = {};
+            out.value = this.format_number(data.valor);
+            if (data.valor != 0) {
+                out.style = `color:${(data.valor < 0) ? this.color.neg : this.color.pos}`;
+            }
+
+            return out;
+        };
+        format.format_number = this.format_number;
+        format.format_number_pct = this.format_number_pct;
+        format.color = this.style;
+        this.format = format;
+
+        return this;
     }
 }
-
-
 
 // Renderiza a tabela
 function table_render(data) {
+    let query = document.querySelectorAll("#receitas-table"),
+        t_det = new table_detalhamento(data),
+        t_cat = new table_categorias(data);
 
-    const table = document.querySelector("#receitas-table");
-    
-    let tr = document.createElement("tr")
-    tr.role = "row"
+    const table_det = query[0],
+        table_cat_receitas = query[1],
+        table_cat_despesas = query[2];
 
-    for (let i = 0; i < 4; i++) {
-        let td = document.createElement("td");
-        td.textContent = i;
-        tr.appendChild(td);
-    }
+    t_det.create_table(table_det);
+    [[table_cat_receitas, "receitas"], [table_cat_despesas, "despesas"]].forEach((value) => {
+        t_cat.create_table(value[0], value[1]);
+        t_cat.create_footer(value[0], value[1]);
+    })
 
-    table.tBodies[0].appendChild(tr);
+
 }
 
 function main() {
-    render(test_data)
-    //table_render(test_data)
+    render(test_data);
+    table_render(test_data);
 }
 
 main()
