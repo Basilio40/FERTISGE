697c29326632025b276689096e323e6c2b67b4b8
sandrobispo <sbispo@otimaeg.com.br>
2023-10-08 15:26:16 -0300

    adcionado configuracao de certificado NFe

--- djangosige/PySIGNFe/exemplos_cte/01_consultar_servidor.py
+++ djangosige/PySIGNFe/exemplos_cte/01_consultar_servidor.py
@@ -11,12 +11,12 @@ if __name__ == '__main__':
     mycte = ct_e()
     
     #Associacao.pfx nao e valido, utilize um certificado valido
-    caminho_certificado = "certificado/Associacao.pfx"
+    caminho_certificado = "certificado/BRASIL FERTILIZANTES E RECICLAGEM LTDA38226805000120.pfx"
     with open(caminho_certificado, 'rb') as f:
         arquivo = f.read()
     
-    info_certificado = mycte.extrair_certificado_a1(arquivo, "associacao")
+    info_certificado = mycte.extrair_certificado_a1(arquivo, "1234")
        
-    resultado = mycte.consultar_servidor(cert=info_certificado['cert'], key=info_certificado['key'], ambiente=2, versao=u'3.00', estado=u'MG', salvar_arquivos=False)
+    resultado = mycte.consultar_servidor(cert=info_certificado['cert'], key=info_certificado['key'], ambiente=2, versao=u'3.00', estado=u'MT', salvar_arquivos=False)
     print('Status: '+str(resultado['status']))
     print('Razao: '+str(resultado['reason']))
